4bba7959d72f171885d6f6c978d804b7e5fa59ad
Lea Beusch <leabeusch@users.noreply.github.com>
2021-02-25 14:17:22 +0100

    Generalize local X y wgt preparation in training (#9)
    * Restructure + generalize preparing X, y, wgt
    
    Take fct to prepare X, y, wgt for equal weighting scenarios out of
    train_lt.py so that can also be used by train_lv later.
    Switch y from dictionary to nd.array for easier + more consistent
    handling.

--- mesmer/calibrate_mesmer/__init__.py
+++ mesmer/calibrate_mesmer/__init__.py
@@ -8,3 +8,4 @@ from .train_gt import *
 from .train_gv import *
 from .train_lt import *
 from .train_lv import *
+from .train_utils import *
---
+++ mesmer/calibrate_mesmer/train_utils.py
@@ -0,0 +1,95 @@
+"""
+mesmer.calibrate_mesmer.train_utils
+===================
+Functions to aid the training of MESMER.
+
+
+Functions:
+    train_l_prepare_X_y_wgteq()
+
+"""
+
+
+import numpy as np
+
+
+def train_l_prepare_X_y_wgteq(preds, targs):
+    """Create single array of predictors, single array of targets, and single array of weights.
+
+    Args:
+    - preds (dict): empty dictionary if none, else nested dictionary of predictors with keys
+        [pred][scen] with 1d/2d arrays (time)/(run,time)
+    - targs (dict): nested dictionary of targets with keys
+        [targ][scen] with 3d arrays (run,time,gp)
+
+    Returns:
+    - X (np.ndarray): empty array if none, else 2d array (sample,pred) of predictors
+    - y (np.ndarray): 3d array (sample,gp,targ) or targets
+    - wgt_scen_eq (np.ndarray): 1d array (sample) of sample weights based on equal treatment of each scenario (if scen has more samples, each sample gets less weight)
+
+    """
+
+    targ_names = list(targs.keys())
+    targ_name = targ_names[0]  # because same approach for each targ
+    pred_names = list(preds.keys())
+
+    # identify characteristics of the predictors and the targets
+    targ = targs[
+        targ_name
+    ]  # predictors are not influenced by whether there is a single or there are multiple targets
+    scens = list(targ.keys())
+
+    # assumption: nr_runs per scen and nr_ts for these runs can vary
+    # derive weights such that each scenario receives same weight (divide by nr samples)
+    nr_samples = 0
+    wgt_scen_eq = []
+    for scen in scens:
+        nr_runs, nr_ts, nr_gps = targ[scen].shape
+        nr_samples_scen = nr_runs * nr_ts
+        wgt_scen_eq = np.append(wgt_scen_eq, np.repeat(1 / nr_runs, nr_samples_scen))
+        nr_samples += nr_samples_scen
+
+    nr_preds = len(pred_names)
+    nr_targs = len(targ_names)
+
+    # derive X (ie array of predictors)
+    if nr_preds == 0:
+        X = np.empty(0)
+    else:
+        X = np.zeros([nr_samples, nr_preds])
+        for p in np.arange(nr_preds):  # index for predictors
+            pred_name = pred_names[p]  # name of predictor p
+            s = 0  # index for samples
+            pred_raw = preds[pred_name]  # values of predictor p
+            for scen in scens:
+                if (
+                    len(pred_raw[scen].shape) == 2
+                ):  # if 1 time series per run for predictor (e.g., gv)
+                    k = (
+                        pred_raw[scen].shape[0] * pred_raw[scen].shape[1]
+                    )  # nr_runs*nr_ts for this specific scenario
+                    X[s : s + k, p] = pred_raw[scen].flatten()
+                    s += k
+                elif (
+                    len(pred_raw[scen].shape) == 1
+                ):  # if single time series as predictor (e.g. gt): repeat ts as many times as runs available
+                    nr_runs, nr_ts, nr_gps = targ[scen].shape
+                    nr_samples_scen = nr_runs * nr_ts
+                    X[s : s + nr_samples_scen, p] = np.tile(pred_raw[scen], nr_runs)
+                    s += nr_samples_scen
+                else:
+                    print("Predictors in this shape cannot be processed.")
+
+    # derive y (ie array of targets)
+    y = np.zeros([nr_samples, nr_gps, nr_targs])
+    for t, targ_name in enumerate(targ_names):
+        targ = targs[targ_name]
+        s = 0
+        for scen in scens:
+            k = (
+                targ[scen].shape[0] * targ[scen].shape[1]
+            )  # nr_runs*nr_ts for this scenario
+            y[s : s + k, :, t] = targ[scen].reshape(k, -1)
+            s += k
+
+    return X, y, wgt_scen_eq
