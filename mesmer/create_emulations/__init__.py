46704a32b851e0577410f54b67aeb2e30e51e874
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2021-03-23 16:32:39 +1100

    More outlines

--- mesmer/create_emulations/__init__.py
+++ mesmer/create_emulations/__init__.py
@@ -25,6 +25,12 @@ def make_realisations(
 
     Parameters
     ----------
+    preds_lt : dict
+        Dictionary where each key is a different variable. The values are
+        themselves dictionaries. These sub-dictionaries have keys which are
+        different scenarios ("hist" separate from scenarios), each of which is
+        a :obj:`np.ndarray` of shape ``(n_timesteps)`` i.e. the length of the
+        scenario.
 
     params_lt : dict
         Description to come
@@ -32,10 +38,24 @@ def make_realisations(
     preds_lv : dict
         Dictionary with a single key, ``gvtas``, whose value is itself a
         dictionary. The value should also have a single key, ``all``, the
-        value for which should be an array of shape (n_realisations,
-        n_timesteps).
+        value for which should be an array of shape
+        ``(n_realisations, n_timesteps)``.
 
-    a
+    params_lv : dict
+        Description to come
+
+    n_realisations : int
+        Number of realisations to draw
+
+    seeds : dict
+        Seeds to use for random number generators. Keys are different climate
+        models, values are themselves dictionaries. Each value has keys which
+        are different scenarios (or ``"all"``) and values which are themselves
+        dictionaries. These final sub-values contain two keys, ``["gv",
+        "lv"]``, whiche define the seeds for the global variability and local
+        variability generators respectively.
+
+    land_fractions
     """
     import pdb
     pdb.set_trace()
