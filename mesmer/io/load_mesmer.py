2879dcb86d61d643347243cdf01879a7b19762fe
Lea Beusch <leabeusch@users.noreply.github.com>
2021-04-28 10:35:32 +0200

    Rewrite load_mesmer_output() (#21)
    * Rewrite load_mesmer_output()
    
    The old version worked with config file entries which are no longer
    available plus was written in a complicated way. The new version works
    for the current MESMER version and is written in a more
    concise and flexible way.
    
    * Fix typo
    
    "emu" -> "emus"
    
    * Add keyword argument mid_path
    
    * Improve compatibility with diff OS and readability
    
    * Add part of documentation I forgot to add before

--- mesmer/io/load_mesmer.py
+++ mesmer/io/load_mesmer.py
@@ -10,162 +10,94 @@ Functions:
 """
 
 
+import glob
+import os.path
+
 import joblib
 
 
-def load_mesmer_output(name, targ_names, esm, cfg, scen_type="emus"):
+def load_mesmer_output(
+    name,
+    cfg,
+    ens_type_str="_",
+    method_str="_",
+    preds_str="_",
+    targs_str="_",
+    esm_str="_",
+    scen_str="",
+    mid_path=None,
+):
+    """Load saved MESMER output (parameters or emulations).
 
-    # assumption: targets which are saved in single mesmer output entity share the applied methods and predictors
+    Args:
+    - name (str): saved MESMER output to load (e.g., 'params_lt', 'emus_lv', 'emus_g')
+    - cfg (module): config file containnig metadata
+    - ens_type_str (str, optional): ensemble type (e.g., 'msic')
+    - method_str (str, optional): method (e.g., 'OLS')
+    - preds_str (st, optional): predictos (e.g., 'gttas', 'gttas_gttas2')
+    - targs_str (str, optional): target variables (e.g., 'tas')
+    - esm_str (str, optional): Earth System Model (e.g., 'CanESM2', 'CanESM5')
+    - scen_str (str, otional): scenario (e.g., 'rcp85', 'ssp585', 'h-ssp585')
+    - mid_path (str, optional): middle part of pathway depending on what exactly want to load (e.g., 'local/local_trends')
 
-    # specify necessary variables from config file
-    ens_type_tr = cfg.ens_type_tr
+    Returns:
+    - dict_out (dict): loaded MESMER output dictionary
+        [xx] depending on the loaded output
 
-    targ = targ_names[0]  # same methods for all targets
-    method_gt = cfg.methods[targ]["gt"]
-    method_gv = cfg.methods[targ]["gv"]
-    method_lt = cfg.methods[targ]["lt"]
-    method_lv = cfg.methods[targ]["lv"]
-    preds_gt = cfg.preds[targ]["gt"]
-    preds_gv = cfg.preds[targ]["gv"]
-    preds_lt = cfg.preds[targ]["lt"]
-    preds_lv = cfg.preds[targ]["lv"]
+    General remarks:
+    - This function can only load a single .pkl file at every call
+    - If multiple files exist for the given input strings, an empty dictionary is returned
+    - If no file exists for the given input strings, an empty dictionary is returned
+    - Also partial strings are accepted (with the exception of esm_str, where the full ESM name is needed): e.g.,
+            scen_str='h-' for joint historical + ssp scenarios
+            scen_str='hist' for separated historical + ssp scenarios
+    - If no mid_path is provided the default MESMER structure for saved params and emus is assumed
 
+    """
+
+    # choose directory depending on whether want to load params or emus
     if "params" in name:
         dir_mesmer = cfg.dir_mesmer_params
-    elif "emus" in name:
+    elif "emu" in name:
         dir_mesmer = cfg.dir_mesmer_emus
 
-    if scen_type == "emus":
-        # scenarios = cfg.scenarios_emus
-        scen_name = cfg.scen_name_emus
-    elif scen_type == "tr":
-        # scenarios = cfg.scenarios_tr
-        scen_name = cfg.scen_name_tr
+    # identify middle part of pathway depending on what exactly want to load if not passed as keyword argument
+    if mid_path is None:
+        if "gt" in name:
+            mid_path = os.path.join("global", "global_trend")
+        elif "gv" in name:
+            mid_path = os.path.join("global", "global_variability")
+        elif "g" in name:
+            mid_path = "global"
+        elif "lt" in name:
+            mid_path = os.path.join("local", "local_trends")
+        elif "lv" in name:
+            mid_path = os.path.join("local", "local_variability")
+        elif "l" in name:
+            mid_path = "local"
 
-    if "gt" in name:
-        file = (
-            "global/global_trend/"
-            + name
-            + "_"
-            + ens_type_tr
-            + "_"
-            + method_gt
-            + "_"
-            + "_".join(preds_gt)
-            + "_"
-            + "_".join(targ_names)
-            + "_"
-            + esm
-            + "_"
-            + scen_name
-            + ".pkl"
+    # to ensure that don't end up with multiple files if multiple ESMs share same beginning of name
+    if esm_str != "_":
+        esm_str = esm_str + "_"
+
+    path_list = glob.glob(
+        os.path.join(
+            dir_mesmer,
+            mid_path,
+            f"{name}*{ens_type_str}*{method_str}*{preds_str}*{targs_str}*{esm_str}*{scen_str}*.pkl",
         )
-    elif "gv" in name:
-        file = (
-            "global/global_variability/"
-            + name
-            + "_"
-            + ens_type_tr
-            + "_"
-            + method_gv
-            + "_"
-            + "_".join(preds_gv)
-            + "_"
-            + "_".join(targ_names)
-            + "_"
-            + esm
-            + "_"
-            + scen_name
-            + ".pkl"
+    )
+
+    # load the requested output dictionary
+    if len(path_list) == 1:
+        dict_out = joblib.load(path_list[0])
+    elif len(path_list) == 0:
+        print("No such file exists. An empty dictionary will be returned.")
+        dict_out = {}
+    elif len(path_list) > 1:
+        print(
+            "More than 1 file exists for these critera. Please be more concrete in your selection. An empty dictionary will be returned"
         )
+        dict_out = {}
 
-    elif "g" in name:
-        file = (
-            "global/"
-            + name
-            + "_"
-            + ens_type_tr
-            + "_gt_"
-            + method_gt
-            + "_"
-            + "_".join(preds_gt)
-            + "_gv_"
-            + method_gv
-            + "_"
-            + "_".join(preds_gv)
-            + "_"
-            + "_".join(targ_names)
-            + "_"
-            + esm
-            + "_"
-            + scen_name
-            + ".pkl"
-        )
-
-    elif "lt" in name:
-        file = (
-            "local/local_trends/"
-            + name
-            + "_"
-            + ens_type_tr
-            + "_"
-            + method_lt
-            + "_"
-            + "_".join(preds_lt)
-            + "_"
-            + "_".join(targ_names)
-            + "_"
-            + esm
-            + "_"
-            + scen_name
-            + ".pkl"
-        )
-
-    elif "lv" in name:
-        file = (
-            "local/local_variability/"
-            + name
-            + "_"
-            + ens_type_tr
-            + "_"
-            + method_lv
-            + "_"
-            + "_".join(preds_lv)
-            + "_"
-            + "_".join(targ_names)
-            + "_"
-            + esm
-            + "_"
-            + scen_name
-            + ".pkl"
-        )
-
-    elif "l" in name:
-        file = (
-            "local/"
-            + name
-            + "_"
-            + ens_type_tr
-            + "_lt_"
-            + method_lt
-            + "_"
-            + "_".join(preds_lt)
-            + "_lv_"
-            + method_lv
-            + "_"
-            + "_".join(preds_lv)
-            + "_"
-            + "_".join(targ_names)
-            + "_"
-            + esm
-            + "_"
-            + scen_name
-            + ".pkl"
-        )
-
-    else:
-        print("No such file exists. No file is loaded.")
-
-    mesmer_output = joblib.load(dir_mesmer + file)
-
-    return mesmer_output
+    return dict_out
