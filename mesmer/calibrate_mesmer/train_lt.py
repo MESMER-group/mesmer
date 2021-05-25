65761e999fa8201a56ab8e86b68c5d03ed59ecef
Mathias Hauser <mathias.hauser@env.ethz.ch>
2021-05-25 16:34:25 +0200

    mention mesmer in header

--- mesmer/calibrate_mesmer/train_lt.py
+++ mesmer/calibrate_mesmer/train_lt.py
@@ -1,3 +1,4 @@
+# MESMER, land-climate dynamics group, S.I. Seneviratne
 # Copyright (c) 2021 ETH Zurich, contributors listed in AUTHORS.
 # Licensed under the GNU General Public License v3.0; see LICENSE or https://www.gnu.org/licenses/
 """
