7003602a0782be0475c456292f159fcfb802c133
Mathias Hauser <mathause@users.noreply.github.com>
2021-05-25 16:50:56 +0200

    Update mesmer/__init__.py

--- mesmer/__init__.py
+++ mesmer/__init__.py
@@ -9,7 +9,7 @@ analyze the results.
 
 # import subpackages (so that if I import mesmer, I can directly access e.g., io content
 # by calling e.g., mesmer.io.load_const())
-from . import calibrate_Create_emulations, io, utils
+from . import calibrate_mesmer, create_emulations, io, utils
 
 # like this could directly access all functions (i.e., if I import mesmer, I can
 # directly access fcts, e.g., mesmer.load_const() decided against it for now to keep the
