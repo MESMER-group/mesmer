cd62c5c47418cf5eb89247eb0c88e484bf1a5ca9
Lea Beusch <lea.beusch@hotmail.com>
2021-06-27 15:32:47 +0200

    Remove full_model key from train_lt.py

--- mesmer/calibrate_mesmer/train_lt.py
+++ mesmer/calibrate_mesmer/train_lt.py
@@ -7,7 +7,6 @@ Functions to train local trends module of MESMER.
 """
 
 
-import copy
 import os
 
 import joblib
@@ -87,6 +86,9 @@ def train_lt(preds, targs, esm, cfg, save_params=True):
 
     # specify necessary variables from config file
     wgt_scen_tr_eq = cfg.wgt_scen_tr_eq
+    method_lt = cfg.methods[targ_name]["lt"]
+    method_lv = cfg.methods[targ_name]["lv"]
+    method_lt_each_gp_sep = cfg.method_lt_each_gp_sep
 
     preds_lt = []
     preds_lv = []
@@ -96,10 +98,6 @@ def train_lt(preds, targs, esm, cfg, save_params=True):
             preds_lt.append(pred)
         elif "gv" in pred:
             preds_lv.append(pred)
-
-    method_lt = cfg.methods[targ_name]["lt"]
-    method_lv = cfg.methods[targ_name]["lv"]
-    method_lt_each_gp_sep = cfg.method_lt_each_gp_sep
 
     scenarios_tr = list(targs[targ_name].keys())
 
@@ -111,32 +109,6 @@ def train_lt(preds, targs, esm, cfg, save_params=True):
     params_lt["method_each_gp_sep"] = method_lt_each_gp_sep
     params_lt["preds"] = preds_lt
     params_lt["scenarios"] = scenarios_tr
-
-    # select the method from a dict of fcts
-    training_method_func_mapping = {"OLS": LinearRegression().fit}
-
-    # dict could be extended to contain other methods (even actual fcts that I write)
-    training_method_lt = training_method_func_mapping[method_lt]
-
-    # prepare predictors and targets such that they can be ingested into the training function
-    X, y, wgt_scen_eq = train_l_prepare_X_y_wgteq(preds, targs)
-
-    # prepare weights for individual runs
-
-    # train the full model + save it (may also contain lv module parts)
-    params_lt["full_model"] = {}
-    if wgt_scen_tr_eq is False:
-        wgt_scen_eq[
-            :
-        ] = 1  # if each scen does not get the same weight, each sample gets it instead
-
-    if method_lt_each_gp_sep:
-        nr_gps = y.shape[1]
-        for gp in np.arange(nr_gps):
-            reg = training_method_lt(X, y[:, gp, :], wgt_scen_eq)
-            params_lt["full_model"][gp] = copy.deepcopy(
-                reg
-            )  # needed because otherwise the last coef everywhere
 
     # check if parameters for local variability module have been derived too, if yes, initialize params_lv
     if method_lt in method_lv:
@@ -155,15 +127,50 @@ def train_lt(preds, targs, esm, cfg, save_params=True):
         params_lt["full_model_contains_lv"] = False
         params_lv = {}  # only initiate empty dictionary
 
-    # select the method to extract additional parameters from a dict of fcts
-    extract_additonal_params_func_mapping = {
-        "OLS": train_lt_extract_additional_params_OLS
-    }
-    # if additional lt methods are integrated, dict needs to be extended
+    # prepare predictors and targets such that they can be ingested into the training function
+    X, y, wgt_scen_eq = train_l_prepare_X_y_wgteq(preds, targs)
 
-    extract_additonal_params_lt = extract_additonal_params_func_mapping[method_lt]
+    # prepare weights for individual runs
+    if wgt_scen_tr_eq is False:
+        wgt_scen_eq[:] = 1
+        # if each scen does not get the same weight, each sample gets it instead
 
-    params_lt, params_lv = extract_additonal_params_lt(params_lt, params_lv)
+    # train the full model + save it (may also contain lv module parts)
+    if method_lt_each_gp_sep and method_lt == "OLS":
+        nr_gps = y.shape[1]
+
+        # initialize the regression coefficient dictionaries
+        params_lt["intercept"] = {}
+        for targ in params_lt["targs"]:
+            params_lt["intercept"][targ] = np.zeros(nr_gps)
+        for pred in params_lt["preds"]:
+            params_lt["coef_" + pred] = {}
+            for targ in params_lt["targs"]:
+                params_lt["coef_" + pred][targ] = np.zeros(nr_gps)
+        if len(params_lv) > 0:
+            for pred in params_lv["preds"]:
+                params_lv["coef_" + pred] = {}
+                for targ in params_lt["targs"]:
+                    params_lv["coef_" + pred][targ] = np.zeros(nr_gps)
+
+        # derive the OLS parameters for each gp
+        for gp in np.arange(nr_gps):
+            reg = LinearRegression().fit(X, y[:, gp, :], wgt_scen_eq)
+
+            for targ_idx, targ in enumerate(params_lt["targs"]):
+                params_lt["intercept"][targ][gp] = reg.intercept_[targ_idx]
+
+                coef_idx = 0  # coefficient index
+                for pred in params_lt["preds"]:
+                    params_lt["coef_" + pred][targ][gp] = reg.coef_[targ_idx, coef_idx]
+                    coef_idx += 1
+
+                if len(preds_lv) > 0:  # assumption: coefs of lv are behind coefs of lt
+                    for pred in params_lv["preds"]:
+                        params_lv["coef_" + pred][targ][gp] = reg.coef_[
+                            targ_idx, coef_idx
+                        ]
+                        coef_idx += 1
 
     # save the local trend paramters if requested
     if save_params:
@@ -208,89 +215,3 @@ def train_lt(preds, targs, esm, cfg, save_params=True):
         return params_lt, params_lv
     else:
         return params_lt
-
-
-def train_lt_extract_additional_params_OLS(params_lt, params_lv):
-    """Extract additional parameters for the ordinary least squares (OLS) method.
-
-    Parameters
-    ----------
-    params_lt : dict
-        dictionary with the trained local trend parameters
-
-        - ["targs"] (emulated variables, str)
-        - ["esm"] (Earth System Model, str)
-        - ["method"] ("OLS")
-        - ["method_each_gp_sep"] (states if method is applied to each grid point
-          separately, bool)
-        - ["preds"] (predictors, list of strs)
-        - ["scenarios"] (emission scenarios used for training, list of strs)
-        - ["full_model"] (full local trends model)
-    params_lv : dict
-        dictionary of local variability paramters which are derived together with the
-        local trend parameters. if len(params_lv)>0 requires
-
-        - ["targs"] (emulated variables, str)
-        - ["esm"] (Earth System Model, str)
-        - ["method"] (applied method, str)
-        - ["preds"] (predictors, list of strs)
-        - ["scenarios"] (emission scenarios used for training, list of strs)
-        - ["part_model_in_lt"] (states if part of the model is saved in params_lt, bool)
-        - ["method_lt_each_gp_sep"] (states if local trends method is applied to each
-          grid point separately, bool)
-
-    Returns
-    -------
-    params_lt : dict
-        local trends parameters dictionary with added keys
-
-        - ["intercept"][targ] (1d array (gp) of intercept terms for each target)
-        - ["coef\\_" + pred][targ] (1d array (gp) of OLS regression coefficient terms
-          for each predictor and each target)
-    params_lv : dict
-        local variability parameters dictionary with added keys. if len(params_lv)>0
-        returns
-
-        - ["coef\\_" + pred][targ] (1d array (gp) of OLS regression coefficient terms
-          for each predictor and each target)
-    """
-    nr_mod_keys = len(params_lt["full_model"])  # often grid points but not necessarily
-    nr_targs = len(params_lt["targs"])
-
-    # initialize the regression coefficient dictionaries
-    params_lt["intercept"] = {}
-    for pred in params_lt["preds"]:
-        params_lt["coef_" + pred] = {}
-    if len(params_lv) > 0:
-        for pred in params_lv["preds"]:
-            params_lv["coef_" + pred] = {}
-
-    # fill the reg coef dictionaries for each target
-    for targ_idx in np.arange(nr_targs):
-        targ = params_lt["targs"][targ_idx]
-
-        params_lt["intercept"][targ] = np.zeros(nr_mod_keys)
-        for mod_key in params_lt["full_model"].keys():
-            params_lt["intercept"][targ][mod_key] = params_lt["full_model"][
-                mod_key
-            ].intercept_[targ_idx]
-
-        coef_idx = 0  # coefficient index
-        for pred in params_lt["preds"]:
-            params_lt["coef_" + pred][targ] = np.zeros(nr_mod_keys)
-            for mod_key in params_lt["full_model"].keys():
-                params_lt["coef_" + pred][targ][mod_key] = params_lt["full_model"][
-                    mod_key
-                ].coef_[targ_idx, coef_idx]
-            coef_idx += 1
-
-        if len(params_lv) > 0:  # assumption: coefs of lv are behind coefs of lt
-            for pred in params_lv["preds"]:
-                params_lv["coef_" + pred][targ] = np.zeros(nr_mod_keys)
-                for mod_key in params_lt["full_model"].keys():
-                    params_lv["coef_" + pred][targ][mod_key] = params_lt["full_model"][
-                        mod_key
-                    ].coef_[targ_idx, coef_idx]
-                coef_idx += 1
-
-    return params_lt, params_lv
