b93de5877fbe84a39f713b6d0fc1cd36ed78eac6
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2021-10-25 13:21:12 +1100

    Tidy up a bit more

--- mesmer/prototype/calibrate.py
+++ mesmer/prototype/calibrate.py
@@ -11,6 +11,17 @@ class MesmerCalibrateBase(metaclass=abc.ABCMeta):
     """
 
     @staticmethod
+    def _get_predictor_dims(predictors):
+        predictors_dims = {k: v.dims for k, v in predictors.items()}
+        predictors_dims_unique = set(predictors_dims.values())
+        if len(predictors_dims_unique) > 1:
+            raise AssertionError(
+                f"Dimensions of predictors are not all the same, we have: {predictors_dims}"
+            )
+
+        return list(predictors_dims_unique)[0]
+
+    @staticmethod
     def _get_stack_coord_name(inp_array):
         stack_coord_name = "stacked_coord"
         if stack_coord_name in inp_array.dims:
@@ -121,17 +132,6 @@ class LinearRegression(MesmerCalibrateTargetPredictor):
 
         return reg
 
-    @staticmethod
-    def _get_predictor_dims(predictors):
-        predictors_dims = {k: v.dims for k, v in predictors.items()}
-        predictors_dims_unique = set(predictors_dims.values())
-        if len(predictors_dims_unique) > 1:
-            raise AssertionError(
-                f"Dimensions of predictors are not all the same, we have: {predictors_dims}"
-            )
-
-        return list(predictors_dims_unique)[0]
-
     def _calibrate_groups(
         self,
         target_flattened,
@@ -157,6 +157,25 @@ class LinearRegression(MesmerCalibrateTargetPredictor):
         res = target_flattened.groupby(calibration_groups).apply(_calibrate_group)
 
         return res
+
+    @staticmethod
+    def _get_predictors_numpy_and_output_predictor_order(
+        predictors_flattened, stack_coord_name
+    ):
+        # stacked coord has to go first for the regression to be setup correctly
+        predictors_flattened_dim_order = [stack_coord_name] + [
+            d for d in predictors_flattened.dims if d != stack_coord_name
+        ]
+        predictors_flattened_reordered = predictors_flattened.transpose(
+            *predictors_flattened_dim_order
+        )
+        predictors_numpy = predictors_flattened_reordered.values
+
+        predictors_plus_intercept_order = ["intercept"] + list(
+            predictors_flattened_reordered["variable"].values
+        )
+
+        return predictors_numpy, predictors_plus_intercept_order
 
     def calibrate(self, target, predictors, weights=None):
         """
@@ -194,17 +213,12 @@ class LinearRegression(MesmerCalibrateTargetPredictor):
             stack_coord_name,
         ) = self._flatten_predictors_and_target(predictors, target)
 
-        # stacked coord has to go first for the regression to be setup correctly
-        predictors_flattened_dim_order = [stack_coord_name] + [
-            d for d in predictors_flattened.dims if d != stack_coord_name
-        ]
-        predictors_flattened_reordered = predictors_flattened.transpose(
-            *predictors_flattened_dim_order
+        (
+            predictors_numpy,
+            predictors_plus_intercept_order,
+        ) = self._get_predictors_numpy_and_output_predictor_order(
+            predictors_flattened, stack_coord_name
         )
-        predictors_plus_intercept = ["intercept"] + list(
-            predictors_flattened_reordered["variable"].values
-        )
-        predictor_numpy = predictors_flattened_reordered.values
 
         calibration_groups = self._get_calibration_groups(
             target_flattened, stack_coord_name
@@ -212,8 +226,8 @@ class LinearRegression(MesmerCalibrateTargetPredictor):
 
         return self._calibrate_groups(
             target_flattened,
-            predictor_numpy,
-            predictor_names=predictors_plus_intercept,
+            predictors_numpy,
+            predictor_names=predictors_plus_intercept_order,
             weights=weights,
             calibration_groups=calibration_groups,
         )
