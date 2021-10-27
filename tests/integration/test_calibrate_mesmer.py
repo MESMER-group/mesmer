f70274bb9f0878e051e2b9525b5b453ebda4dce7
Mathias Hauser <mathause@users.noreply.github.com>
2021-10-27 19:03:17 +0200

    refactor load_cmipng_tas and load_cmipng_hfds (#55)
    * refactor: load_cmipng_tas/ hfds
    
    * black
    
    * rename load_cmipng.py
    
    * add initial test
    
    * Apply suggestions from code review
    
    * move _check_dict to own file
    
    * add tests for data loading
    
    * add changelog
    
    * fix extra lines
    
    * Apply suggestions from code review

--- tests/integration/test_calibrate_mesmer.py
+++ tests/integration/test_calibrate_mesmer.py
@@ -2,37 +2,10 @@ import os.path
 import shutil
 
 import joblib
-import numpy as np
-import numpy.testing as npt
-import xarray as xr
-import xarray.testing as xrt
 
 from mesmer.calibrate_mesmer import _calibrate_and_draw_realisations
 
-
-def _check_dict(first, second, first_name, second_name):
-    for k in first:
-        first_val = first[k]
-        try:
-            second_val = second[k]
-        except KeyError:
-            raise AssertionError(
-                "Key `{}` is in '{}' but is not in '{}'".format(
-                    k, first_name, second_name
-                )
-            )
-
-        assert type(first_val) == type(second_val)
-        if isinstance(first_val, dict):
-            _check_dict(first_val, second_val, first_name, second_name)
-        elif isinstance(first_val, np.ndarray):
-            npt.assert_allclose(first_val, second_val)
-        elif isinstance(first_val, xr.DataArray):
-            xrt.assert_allclose(first_val, second_val)
-        elif np.issubdtype(np.array(first_val).dtype, np.number):
-            npt.assert_allclose(first_val, second_val)
-        else:
-            assert first_val == second_val, k
+from .utils import _check_dict
 
 
 def test_calibrate_mesmer(test_data_root_dir, tmpdir, update_expected_files):
