b901cc251c08669a68131de246b37b971de12f82
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2021-11-30 12:36:44 +1100

    Pass unit tests

---
+++ mesmer/core/linear_regression.py
@@ -0,0 +1,28 @@
+from sklearn.linear_model import LinearRegression
+
+
+def linear_regression(predictors, target, weights=None):
+    """
+    Perform a linear regression
+
+    Parameters
+    ----------
+    predictors : array-like of shape (n_samples, n_predictors)
+        Array of predictors
+
+    target : array-like of shape (n_samples,)
+        Array of targets
+
+    weights : array-like of shape (n_samples,)
+        Weights for each sample
+
+    Returns
+    -------
+    :obj:`np.ndarray`
+        Intercept of regression followed by the intercept for each predictor (
+        in the same order as the columns of ``predictors``)
+    """
+    reg = LinearRegression()
+    reg.fit(X=predictors, y=target, sample_weight=weights)
+
+    return reg.get_params()
--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -9,7 +9,14 @@ import mesmer.core.linear_regression
     ([[1, 2, 3], [2, 4, 0]], [1, 2], [3, 1]),
 ))
 def test_linear_regression(predictors, target, weight):
+    # This testing is really nasty because the function is (deliberately)
+    # written without proper dependency injection. See e.g.
+    # https://stackoverflow.com/a/46865495 which recommends against this
+    # approach. At the moment, I can't see how to write a suitably simple
+    # function for regressions without making the interface more complicated.
+    mock_regressor = mock.Mock()
     with mock.patch("mesmer.core.linear_regression.LinearRegression") as mocked_linear_regression:
+        mocked_linear_regression.return_value = mock_regressor
 
         if weight is None:
             expected_weights = None
@@ -20,10 +27,11 @@ def test_linear_regression(predictors, target, weight):
 
         mocked_linear_regression.assert_called_once()
         mocked_linear_regression.assert_called_with()
-        mocked_linear_regression.fit.assert_called_once()
-        mocked_linear_regression.fit.assert_called_with(
+        mock_regressor.fit.assert_called_once()
+        mock_regressor.fit.assert_called_with(
             X=predictors, y=target, sample_weight=expected_weights
         )
+        mock_regressor.get_params.assert_called_once()
 
 
 @pytest.mark.parametrize("predictors,target", (
@@ -31,7 +39,7 @@ def test_linear_regression(predictors, target, weight):
     ([[1, 2, 3], [2, 4, 0]], [1, 2, 2]),
 ))
 def test_bad_shape(predictors, target):
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match="inconsistent numbers of samples"):
         mesmer.core.linear_regression.linear_regression(predictors, target)
 
 
@@ -40,8 +48,9 @@ def test_bad_shape(predictors, target):
     ([[1, 2, 3], [2, 4, 0]], [1, 2], [3, 1, 1]),
 ))
 def test_bad_shape_weights(predictors, target, weight):
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match="sample_weight.shape.*expected"):
         mesmer.core.linear_regression.linear_regression(predictors, target, weight)
 
 # - results (integration)
 # - results with weights (integration)
+# - results order of coefficients swaps if inputs swap
