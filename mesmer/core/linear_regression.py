869a7df791b6283af1f3869df3df1204d97ccb5a
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2021-12-06 11:13:35 +1100

    Switch to 2d fitting

--- mesmer/core/linear_regression.py
+++ mesmer/core/linear_regression.py
@@ -11,19 +11,26 @@ def linear_regression(predictors, target, weights=None):
     predictors : array-like of shape (n_samples, n_predictors)
         Array of predictors
 
-    target : array-like of shape (n_samples,)
-        Array of targets
+    target : array-like of shape (n_samples, n_targets)
+        Array of targets where each row is a sample and each column is a
+        different target i.e. variable to be predicted
 
     weights : array-like of shape (n_samples,)
         Weights for each sample
 
     Returns
     -------
-    :obj:`np.ndarray`
-        Intercept of regression followed by the intercept for each predictor (
-        in the same order as the columns of ``predictors``)
+    :obj:`np.ndarray` of shape (n_targets, n_predictors + 1)
+        Array of intercepts and coefficients. Each row is the intercept and
+        coefficients for a different target (rows are in same order as the
+        columns of ``target``). In each row, the intercept of the regression is
+        followed by the intercept for each predictor (in the same order as the
+        columns of ``predictors``).
     """
     reg = LinearRegression()
     reg.fit(X=predictors, y=target, sample_weight=weights)
 
-    return np.hstack([reg.intercept_, reg.coef_])
+    intercepts = np.atleast_2d(reg.intercept_).T
+    coefficients = np.atleast_2d(reg.coef_)
+
+    return np.hstack([intercepts, coefficients])
