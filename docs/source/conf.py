bc7ed28e21bc6ed7ac33b2ca775c75d44265e337
Zeb Nicholls <zebedee.nicholls@climate-energy-college.org>
2021-12-23 21:02:17 +1100

    Fix copyright in docs (#127)
    * Fix copyright in docs
    
    * CHANGELOG
    
    * Try removing path hacking

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,26 +4,25 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
+# -- Import packages ---------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-
-# import sys
-# sys.path.insert(0, os.path.abspath("."))
-
+import datetime
+from mesmer._version import get_versions
 
 # -- Project information -----------------------------------------------------
 
 project = "mesmer"
-copyright = "(c) 2021 ETH Zurich (Land-climate dynamics group, Prof. S.I. Seneviratne)"
-author = "Lea Beusch, Zebedee Nicholls, Mathias Hauser"
+copyright_year = datetime.date.today().year
+copyright = "(c) 2021-{} ETH Zurich (Land-climate dynamics group, Prof. S.I. Seneviratne), MESMER contributors listed in AUTHORS".format(
+    copyright_year
+)
+authors = "Authors, see AUTHORS"
+author = authors
 
+# The short X.Y version
+version = get_versions()["version"].split("+")[0]
 # The full version, including alpha/beta/rc tags
-release = "dev"
+release = get_versions()["version"]
 
 
 # -- General configuration ---------------------------------------------------
