c1825ebce633ea80894a6749c5229d21c4d2102b
Mathias Hauser <mathias.hauser@env.ethz.ch>
2022-01-20 11:15:25 +0100

    add py39

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
     "Topic :: Scientific/Engineering",
     "Topic :: Scientific/Engineering :: Atmospheric Science",
 ]
