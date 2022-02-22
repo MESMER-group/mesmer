459421d6483ef4422ccfc365b49553e40a0a1922
Mathias Hauser <mathause@users.noreply.github.com>
2022-02-22 23:06:11 +0100

    add __init__.py for core (#133)
    * add __init__.py for core
    
    * noqa

--- mesmer/__init__.py
+++ mesmer/__init__.py
@@ -13,10 +13,4 @@ del get_versions
 
 # flake8: noqa
 
-# import subpackages (so that if I import mesmer, I can directly access e.g., io content
-# by calling e.g., mesmer.io.load_const())
 from . import calibrate_mesmer, create_emulations, io, utils
-
-# like this could directly access all functions (i.e., if I import mesmer, I can
-# directly access fcts, e.g., mesmer.load_const() decided against it for now to keep the
-# package structure more visible) from .io import *
