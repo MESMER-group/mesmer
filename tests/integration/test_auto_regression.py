2b3b082e9ee79d403fea924be84f8b608d1d31dc
Mathias Hauser <mathias.hauser@env.ethz.ch>
2022-06-16 22:44:31 +0200

    add tests

--- tests/integration/test_auto_regression.py
+++ tests/integration/test_auto_regression.py
@@ -10,6 +10,99 @@ from mesmer.core.utils import _check_dataarray_form, _check_dataset_form
 from .utils import trend_data_1D, trend_data_2D
 
 
+@pytest.mark.parametrize("ar_order", [1, 8])
+@pytest.mark.parametrize("n_cells", [1, 10])
+@pytest.mark.parametrize("n_samples", [2, 5])
+@pytest.mark.parametrize("n_ts", [3, 7])
+def test_draw_auto_regression_np_shape(ar_order, n_cells, n_samples, n_ts):
+
+    intercept = np.zeros(n_cells)
+    coefs = np.ones((ar_order, n_cells))
+    covariance = np.ones((n_cells, n_cells))
+
+    result = mesmer.core.auto_regression._draw_auto_regression_np(
+        intercept=intercept,
+        coefs=coefs,
+        covariance=covariance,
+        n_samples=n_samples,
+        n_ts=n_ts,
+        seed=0,
+        buffer=10,
+    )
+
+    expected_shape = (n_samples, n_ts, n_cells)
+
+    assert result.shape == expected_shape
+
+
+@pytest.mark.parametrize("intercept", [0, 1, 3.14])
+def test_draw_auto_regression_deterministic_intercept(intercept):
+
+    result = mesmer.core.auto_regression._draw_auto_regression_np(
+        intercept=intercept,
+        coefs=np.array([[0]]),
+        covariance=[0],
+        n_samples=1,
+        n_ts=3,
+        seed=0,
+        buffer=10,
+    )
+
+    expected = np.full((1, 3, 1), intercept)
+
+    np.testing.assert_equal(result, expected)
+
+
+def test_draw_auto_regression_deterministic_coefs_buffer():
+
+    result = mesmer.core.auto_regression._draw_auto_regression_np(
+        intercept=1,
+        coefs=np.array([[1]]),
+        covariance=[0],
+        n_samples=1,
+        n_ts=4,
+        seed=0,
+        buffer=1,
+    )
+
+    expected = np.arange(4).reshape(1, -4, 1)
+
+    np.testing.assert_equal(result, expected)
+
+    expected = np.array([0, 1, 1.5, 1.75, 1.875]).reshape(1, -1, 1)
+
+    for i, buffer in enumerate([1, 2]):
+        result = mesmer.core.auto_regression._draw_auto_regression_np(
+            intercept=1,
+            coefs=np.array([[0.5]]),
+            covariance=[0],
+            n_samples=1,
+            n_ts=4,
+            seed=0,
+            buffer=buffer,
+        )
+
+        np.testing.assert_allclose(result, expected[:, i : i + 4])
+
+
+def test_draw_auto_regression_random():
+
+    result = mesmer.core.auto_regression._draw_auto_regression_np(
+        intercept=1,
+        coefs=np.array([[0.375], [0.125]]),
+        covariance=0.5,
+        n_samples=1,
+        n_ts=4,
+        seed=0,
+        buffer=3,
+    )
+
+    expected = np.array([2.58455078, 3.28976946, 1.86569258, 2.78266986])
+    expected = expected.reshape(1, 4, 1)
+
+    np.testing.assert_allclose(result, expected)
+
+
 @pytest.mark.parametrize("obj", [xr.Dataset(), None])
 def test_fit_auto_regression_xr_errors(obj):
 
