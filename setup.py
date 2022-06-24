59dec55a4f6dfe4a5013dd61c66fbf49e2df2037
Mathias Hauser <mathias.hauser@env.ethz.ch>
2022-06-24 09:57:31 +0200

    remove TestCommand class

--- setup.py
+++ setup.py
@@ -1,22 +1,5 @@
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
 
 import versioneer
 
-
-class Mesmer(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-
-        pytest.main(self.test_args)
-
-
-cmdclass = versioneer.get_cmdclass()
-cmdclass.update({"test": Mesmer})
-
-setup(version=versioneer.get_version(), cmdclass=cmdclass)
+setup(version=versioneer.get_version(), cmdclass=versioneer.get_cmdclass())
