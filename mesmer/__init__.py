e5b25c5b2bf344423cf32920e49e0f4fe029dbba
Mathias Hauser <mathias.hauser@env.ethz.ch>
2022-08-12 16:27:32 +0200

    fix __version__ in __init__

--- mesmer/__init__.py
+++ mesmer/__init__.py
@@ -6,11 +6,20 @@
 The mesmer package provides tools to train the MESMER emulator, create emulations, and
 analyze the results.
 """
-from ._version import get_versions
-
-__version__ = get_versions()["version"]
-del get_versions
-
 # flake8: noqa
 
 from . import calibrate_mesmer, create_emulations, io, utils
+
+try:
+    from importlib.metadata import version as _get_version
+except ImportError:
+    # importlib.metadata not available in python 3.7
+    from importlib_metadata import version as _get_version  # type: ignore[no-redef]
+
+try:
+    __version__ = _get_version("mesmer-emulator")
+except Exception:
+    # Local copy or not installed with setuptools.
+    # Disable minimum version checks on downstream libraries.
+    __version__ = "999"
+
