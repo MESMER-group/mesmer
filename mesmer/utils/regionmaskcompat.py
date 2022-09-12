c1418cae3a65cfa4d0d8c6370925204368e6d7ce
Mathias Hauser <mathias.hauser@env.ethz.ch>
2022-09-12 23:52:51 +0200

    mask_percentage: fix coords and pole behaviour

--- mesmer/utils/regionmaskcompat.py
+++ mesmer/utils/regionmaskcompat.py
@@ -2,6 +2,7 @@
 # see licenses/REGIONMASK_LICENSE
 
 import numpy as np
+import regionmask
 import xarray as xr
 
 
@@ -17,10 +18,22 @@ def mask_percentage(regions, lon, lat, **kwargs):
 
     """
 
+    backend = regionmask.core.mask._determine_method(lon, lat)
+    if "rasterize" not in backend:
+        raise ValueError("'lon' and 'lat' must be 1D and equally spaced.")
+
+    if np.min(lat) < -90 or np.max(lat) > 90:
+        raise ValueError("lat must be between -90 and +90")
+
+    lon_name = getattr(lon, "name", "lon")
+    lat_name = getattr(lat, "name", "lat")
+
     lon_sampled = sample_coord(lon)
     lat_sampled = sample_coord(lat)
 
     mask = regions.mask(lon_sampled, lat_sampled, **kwargs)
+
+    sel = (mask.lat >= -90) & (mask.lat <= 90)
 
     isnan = np.isnan(mask.values)
 
@@ -30,14 +43,27 @@ def mask_percentage(regions, lon, lat, **kwargs):
     mask_sampled = list()
     for num in numbers:
         # coarsen the mask again
-        mask_coarse = (mask == num).coarsen(lat=10, lon=10).mean()
+        mask_coarse = mask == num
+        # set points beyond 90° to NaN so we get the correct fraction
+        mask_coarse = mask_coarse.where(sel)
+        mask_coarse = mask_coarse.coarsen(lat=10, lon=10).mean()
         mask_sampled.append(mask_coarse)
 
     mask_sampled = xr.concat(
         mask_sampled, dim="region", compat="override", coords="minimal"
     )
 
-    mask_sampled = mask_sampled.assign_coords(region=("region", numbers))
+    abbrevs = regions[numbers].abbrevs
+    names = regions[numbers].names
+
+    coords = {
+        "region": numbers,
+        lon_name: lon,
+        lat_name: lat,
+        "abbrevs": ("region", abbrevs),
+        "names": ("region", names),
+    }
+    mask_sampled = mask_sampled.assign_coords(coords)
 
     return mask_sampled
 
@@ -52,9 +78,12 @@ def sample_coord(coord):
     -> prototype of what will eventually be integrated in his regionmask package
 
     """
+
+    coord = np.asarray(coord)
+
     d_coord = coord[1] - coord[0]
 
-    n_cells = len(coord)
+    n_cells = coord.size
 
     left = coord[0] - d_coord / 2 + d_coord / 20
     right = coord[-1] + d_coord / 2 - d_coord / 20
