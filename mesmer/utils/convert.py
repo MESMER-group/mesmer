7a822c944fb6df6246df3bbebd0e4d2dfcb193e0
Mathias Hauser <mathause@users.noreply.github.com>
2023-01-19 17:56:46 +0100

    Refactor separate_hist_future (#244)
    * separate_hist_future: small refactor
    
    * preserve the order of the scenarios
    
    * more refactorings

--- mesmer/utils/convert.py
+++ mesmer/utils/convert.py
@@ -91,24 +91,23 @@ def separate_hist_future(var_c, time_c, cfg):
     if gen == 6:
         end_year_hist = 2014
 
-    scen_c = scens_c[0]
-    scen_f = scens_f[0]
-    idx_start_fut = np.where(time_c[scen_c] == end_year_hist)[0][0] + 1
+    # assuming all time vectors are the same
+    time = time_c[scens_c[0]]
 
-    time_s = {}
-    time_s["hist"] = time_c[scen_c][:idx_start_fut]
+    idx_start_fut = np.where(time == end_year_hist)[0][0] + 1
 
-    var_s = {}
-    var_s["hist"] = var_c[scen_c][:, :idx_start_fut]
-    var_s[scen_f] = var_c[scen_c][:, idx_start_fut:]
-    time_s[scen_f] = time_c[scen_c][idx_start_fut:]
-    for scen_f, scen_c in zip(scens_f[1:], scens_c[1:]):
-        # stack all available historical runs
-        var_s["hist"] = np.vstack([var_s["hist"], var_c[scen_c][:, :idx_start_fut]])
-        var_s[scen_f] = var_c[scen_c][:, idx_start_fut:]
-        time_s[scen_f] = time_c[scen_c][idx_start_fut:]
+    var_s, time_s = {}, {}
+
+    # gather hist
+    hist = [var_c[scen_c][:, :idx_start_fut] for scen_c in scens_c]
 
     # exclude duplicate historical runs that are available in several scenarios
-    var_s["hist"] = np.unique(var_s["hist"], axis=0)
+    var_s["hist"] = np.unique(np.vstack(hist), axis=0)
+    time_s["hist"] = time[:idx_start_fut]
+
+    # gather proj
+    for scen_f, scen_c in zip(scens_f, scens_c):
+        var_s[scen_f] = var_c[scen_c][:, idx_start_fut:]
+        time_s[scen_f] = time_c[scen_c][idx_start_fut:]
 
     return var_s, time_s
