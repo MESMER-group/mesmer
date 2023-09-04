e6013a07aac47b772f30ff0cf3b6fc1c3935b353
Mathias Hauser <mathause@users.noreply.github.com>
2023-09-04 19:32:29 +0200

    example: rename more variables and rename config file (#279)
    * add test to train and emulate (legacy)
    
    * linting
    
    * use create_seed_dict function
    
    * update
    
    * mention path updates
    
    * update comments
    
    * rename config file
    
    * rename more vars
    
    * remove test file again

--- examples/train_create_emus_automated.py
+++ examples/train_create_emus_automated.py
@@ -15,10 +15,7 @@ from mesmer.io import load_cmipng, load_phi_gc, load_regs_ls_wgt_lon_lat
 from mesmer.utils import convert_dict_to_arr, extract_land, separate_hist_future
 
 
-def main():
-
-    # load in configurations used in this script
-    import config_across_scen_T_cmip6ng_test as cfg
+def main(cfg):
 
     # specify the target variable
     targ = cfg.targs[0]
@@ -88,12 +85,12 @@ def main():
             "- Prepare predictors for global variability, local trends and variability"
         )
 
-        GSAT_s, time_s = separate_hist_future(gsat[esm], time[esm], cfg)
+        gsat_s, _ = separate_hist_future(gsat[esm], time[esm], cfg)
         gv_novolc_tas_s = {}
         for scen in gt_tas_s.keys():
-            gv_novolc_tas_s[scen] = GSAT_s[scen] - gt_tas_s[scen]
+            gv_novolc_tas_s[scen] = gsat_s[scen] - gt_tas_s[scen]
 
-        tas_s, time_s = separate_hist_future(tas[esm], time[esm], cfg)
+        tas_s, _ = separate_hist_future(tas[esm], time[esm], cfg)
 
         print("- Start with global variability module")
 
@@ -103,12 +100,12 @@ def main():
         scen = list(gt_tas.keys())[0]
         time_v["all"] = time[esm][scen]
         preds_gv = {"time": time_v}
-        emus_gv_T = create_emus_gv(params_gv_tas, preds_gv, cfg, save_emus=True)
+        emus_gv_tas = create_emus_gv(params_gv_tas, preds_gv, cfg, save_emus=True)
 
         # create and save full global emulations
         print("- Merge the global trend and the global variability.")
         create_emus_g(
-            gt_tas, emus_gv_T, params_gt_tas, params_gv_tas, cfg, save_emus=True
+            gt_tas, emus_gv_tas, params_gt_tas, params_gv_tas, cfg, save_emus=True
         )
 
         print("- Start with local trends module")
@@ -154,7 +151,7 @@ def main():
         )
 
         # create full lv emulations
-        preds_lv = {"gvtas": emus_gv_T}  # predictors_list
+        preds_lv = {"gvtas": emus_gv_tas}  # predictors_list
         emus_lv = create_emus_lv(params_lv, preds_lv, cfg, save_emus=True)
 
         # create and save full emulations
@@ -163,4 +160,8 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+
+    # load in configurations used in this script
+    import config_tas_cmip6ng_example as cfg
+
+    main(cfg)
