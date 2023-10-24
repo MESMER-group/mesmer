883c56f15c515d16937d9ee2b26bd153f05ee5fe
Mathias Hauser <mathias.hauser@env.ethz.ch>
2023-10-24 15:59:43 +0200

    add tests

--- tests/unit/test_auto_regression.py
+++ tests/unit/test_auto_regression.py
@@ -1,6 +1,7 @@
 from unittest import mock
 
 import numpy as np
+import pandas as pd
 import pytest
 import xarray as xr
 
@@ -71,6 +72,236 @@ def test_select_ar_order_np():
 
     with pytest.raises(ValueError):
         mesmer.stats.auto_regression._select_ar_order_np(data[:6], 5)
+
+
+@pytest.fixture
+def ar_params_1D():
+
+    intercept = xr.DataArray(0)
+    coeffs = xr.DataArray([0], dims="lags")
+    variance = xr.DataArray(0)
+    ar_params = xr.Dataset(
+        {"intercept": intercept, "coeffs": coeffs, "variance": variance}
+    )
+
+    return ar_params
+
+
+@pytest.fixture
+def ar_params_2D():
+
+    intercept = xr.DataArray([0, 0], dims="gridcell")
+    coeffs = xr.DataArray([[0, 0]], dims=("lags", "gridcell"))
+    variance = xr.DataArray([0, 0], dims="gridcell")
+    ar_params = xr.Dataset(
+        {"intercept": intercept, "coeffs": coeffs, "variance": variance}
+    )
+
+    return ar_params
+
+
+@pytest.fixture
+def covariance():
+
+    data = np.eye(2)
+
+    covariance = xr.DataArray(data, dims=("gridcell_i", "gridcell_j"))
+
+    return covariance
+
+
+@pytest.mark.parametrize("drop", ("intercept", "coeffs", "variance"))
+def test_draw_auto_regression_uncorrelated_wrong_input(ar_params_1D, drop):
+
+    ar_params = ar_params_1D.drop_vars(drop)
+
+    with pytest.raises(
+        ValueError, match=f"ar_params is missing the required data_vars: {drop}"
+    ):
+
+        mesmer.stats.auto_regression._draw_auto_regression_uncorrelated(
+            ar_params,
+            time=1,
+            realisation=1,
+            seed=0,
+            buffer=0,
+        )
+
+
+def test_draw_auto_regression_uncorrelated_2D_errors(ar_params_2D):
+
+    with pytest.raises(
+        ValueError,
+        match="``_draw_auto_regression_uncorrelated`` can currently only handle single points",
+    ):
+
+        mesmer.stats.auto_regression._draw_auto_regression_uncorrelated(
+            ar_params_2D,
+            time=1,
+            realisation=1,
+            seed=0,
+            buffer=0,
+        )
+
+
+@pytest.mark.parametrize("time", (3, 5))
+@pytest.mark.parametrize("realization", (2, 4))
+@pytest.mark.parametrize("time_dim", ("time", "ts"))
+@pytest.mark.parametrize("realization_dim", ("realization", "sample"))
+def test_draw_auto_regression_uncorrelated(
+    ar_params_1D, time, realization, time_dim, realization_dim
+):
+
+    result = mesmer.stats.auto_regression._draw_auto_regression_uncorrelated(
+        ar_params_1D,
+        time=time,
+        realisation=realization,
+        seed=0,
+        buffer=0,
+        time_dim=time_dim,
+        realisation_dim=realization_dim,
+    )
+
+    _check_dataarray_form(
+        result,
+        "result",
+        ndim=2,
+        required_dims={time_dim, realization_dim},
+        shape=(time, realization),
+    )
+
+
+@pytest.mark.parametrize("dim", ("time", "realisation"))
+@pytest.mark.parametrize("wrong_coords", (None, 2.0, np.array([1, 2]), xr.Dataset()))
+def test_draw_auto_regression_uncorrelated_wrong_coords(
+    ar_params_1D, dim, wrong_coords
+):
+
+    coords = {"time": 2, "realisation": 3}
+
+    coords[dim] = wrong_coords
+
+    with pytest.raises(
+        TypeError,
+        match=f"expected '{dim}' to be an `int`, pandas or xarray Index or a `DataArray`",
+    ):
+        mesmer.stats.auto_regression._draw_auto_regression_uncorrelated(
+            ar_params_1D,
+            **coords,
+            seed=0,
+            buffer=0,
+        )
+
+
+@pytest.mark.parametrize("dim", ("time", "realisation"))
+@pytest.mark.parametrize("coords", (xr.DataArray([3, 5]), pd.Index([1, 2, 3])))
+def test_draw_auto_regression_uncorrelated_coords(ar_params_1D, dim, coords):
+
+    coords_ = {"time": 2, "realisation": 3}
+
+    coords_[dim] = coords
+
+    result = mesmer.stats.auto_regression._draw_auto_regression_uncorrelated(
+        ar_params_1D,
+        **coords_,
+        seed=0,
+        buffer=0,
+    )
+
+    assert result[dim].size == coords.size
+    np.testing.assert_equal(result[dim].values, coords.values)
+
+
+@pytest.mark.parametrize("drop", ("intercept", "coeffs"))
+def test_draw_auto_regression_correlated_wrong_input(ar_params_2D, covariance, drop):
+
+    ar_params = ar_params_2D.drop_vars(drop)
+
+    with pytest.raises(
+        ValueError, match=f"ar_params is missing the required data_vars: {drop}"
+    ):
+
+        mesmer.stats.auto_regression._draw_auto_regression_correlated(
+            ar_params,
+            covariance,
+            time=1,
+            realisation=1,
+            seed=0,
+            buffer=0,
+        )
+
+
+@pytest.mark.parametrize("time", (3, 5))
+@pytest.mark.parametrize("realization", (2, 4))
+@pytest.mark.parametrize("time_dim", ("time", "ts"))
+@pytest.mark.parametrize("realization_dim", ("realization", "sample"))
+def test_draw_auto_regression_correlated(
+    ar_params_2D, covariance, time, realization, time_dim, realization_dim
+):
+
+    result = mesmer.stats.auto_regression._draw_auto_regression_correlated(
+        ar_params_2D,
+        covariance,
+        time=time,
+        realisation=realization,
+        seed=0,
+        buffer=0,
+        time_dim=time_dim,
+        realisation_dim=realization_dim,
+    )
+
+    n_gridcells = ar_params_2D.intercept.size
+
+    _check_dataarray_form(
+        result,
+        "result",
+        ndim=3,
+        required_dims={time_dim, "gridcell", realization_dim},
+        shape=(time, n_gridcells, realization),
+    )
+
+
+@pytest.mark.parametrize("dim", ("time", "realisation"))
+@pytest.mark.parametrize("wrong_coords", (None, 2.0, np.array([1, 2]), xr.Dataset()))
+def test_draw_auto_regression_correlated_wrong_coords(
+    ar_params_2D, covariance, dim, wrong_coords
+):
+
+    coords = {"time": 2, "realisation": 3}
+
+    coords[dim] = wrong_coords
+
+    with pytest.raises(
+        TypeError,
+        match=f"expected '{dim}' to be an `int`, pandas or xarray Index or a `DataArray`",
+    ):
+        mesmer.stats.auto_regression._draw_auto_regression_correlated(
+            ar_params_2D,
+            covariance,
+            **coords,
+            seed=0,
+            buffer=0,
+        )
+
+
+@pytest.mark.parametrize("dim", ("time", "realisation"))
+@pytest.mark.parametrize("coords", (xr.DataArray([3, 5]), pd.Index([1, 2, 3])))
+def test_draw_auto_regression_correlated_coords(ar_params_2D, covariance, dim, coords):
+
+    coords_ = {"time": 2, "realisation": 3}
+
+    coords_[dim] = coords
+
+    result = mesmer.stats.auto_regression._draw_auto_regression_correlated(
+        ar_params_2D,
+        covariance,
+        **coords_,
+        seed=0,
+        buffer=0,
+    )
+
+    assert result[dim].size == coords.size
+    np.testing.assert_equal(result[dim].values, coords.values)
 
 
 @pytest.mark.parametrize("ar_order", [1, 8])
