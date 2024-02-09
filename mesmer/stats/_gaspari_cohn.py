f5c7cbd3727734153a51e955d5e78bb5edb56ad2
veni-vidi-vici-dormivi <victoria.bauer@env.ethz.ch>
2024-02-09 12:15:00 +0100

    tweaked formatting of refactored gc

--- mesmer/stats/_gaspari_cohn.py
+++ mesmer/stats/_gaspari_cohn.py
@@ -94,24 +94,22 @@ def _gaspari_cohn_np(r):
     sel = (r >= 0) & (r < 1)
     r_sel = r[sel]
 
-    # fmt: off
+    # NOTE: see https://github.com/MESMER-group/mesmer/issues/157
+
     out[sel] = (
-        1 - r_sel**2 *
-        (5 / 3 - r_sel *
-         (5 / 8  + r_sel *
-          (1 / 2 - 1 / 4 * r_sel)))
+        #fmt: off
+        1 - r_sel**2 * (5 / 3 - r_sel * (5 / 8  + r_sel * (1 / 2 - 1 / 4 * r_sel)))
+        #fmt: on
     )
-    # fmt: on
 
     # compute for 1 <= r < 2
     sel = (r >= 1) & (r < 2)
     r_sel = r[sel]
 
     out[sel] = (
-        4
-        - r_sel
-        * (5 - r_sel * (5 / 3 + r_sel * (5 / 8 - r_sel * (1 / 2 - 1 / 12 * r_sel))))
-        - 2 / (3 * r_sel)
+        #fmt: off
+        4 - r_sel * (5 - r_sel * (5 / 3 + r_sel * (5 / 8 - r_sel * (1 / 2 - 1 / 12 * r_sel)))) - 2 / (3 * r_sel)
+        #fmt: on
     )
 
     return out
