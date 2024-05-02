bebeec019638b35e9328c4f87ca6bd9637159084
veni-vidi-vici-dormivi <victoria.bauer@env.ethz.ch>
2024-05-02 16:20:12 +0200

    fixes temporary spatial

--- mesmer/mesmer_x/temporary_spatial.py
+++ mesmer/mesmer_x/temporary_spatial.py
@@ -7,7 +7,7 @@ import numpy as np
 import pyproj
 import xarray as xr
 
-# from .utils import create_equal_dim_names
+from mesmer.core.utils import create_equal_dim_names
 
 
 # TAKEN FROM CURRENT CODE OF MESMER, TO REMOVE WITH REBASE
