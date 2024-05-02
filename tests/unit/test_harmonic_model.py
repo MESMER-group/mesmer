c10b73fa121b595d4a2bb200cb0e20e2920cae89
veni-vidi-vici-dormivi <victoria.bauer@env.ethz.ch>
2024-05-02 11:45:42 +0200

    generalise tests

--- tests/unit/test_harmonic_model.py
+++ tests/unit/test_harmonic_model.py
@@ -21,14 +21,21 @@ def test_generate_fourier_series_np():
 
 
 @pytest.mark.parametrize(
-    "coefficients", [np.array([0, -1, 0, -2]), np.array([0, -1, 0, -2, 0, 3, 0, -4])]
+    "coefficients", [np.array([0, -1, 0, -2]),
+                     np.array([1,2,3,4,5,6,7,8]),
+                     np.array([1,2,3,4,5,6,7,8,9,10,11,12]),],
 )
-def test_fit_to_bic_np(coefficients):
-    n_years = 10
-    n_months = n_years * 12
+@pytest.mark.parametrize(
+    "yearly_predictor", [np.zeros(10*12), 
+                         np.ones(10*12),
+                         np.linspace(-1, 1, 10*12)*10],
+)
+def test_fit_to_bic_np(coefficients, yearly_predictor):
 
-    yearly_predictor = np.zeros(n_months)
-    months = np.tile(np.arange(1, 13), n_years)
+    # fill up all coefficient arrays with zeros to have the same length 4*6
+    coefficients = np.concatenate([coefficients, np.zeros(4*6-len(coefficients))])
+
+    months = np.tile(np.arange(1, 13), 10)
 
     monthly_target = generate_fourier_series_np(
         coefficients, int(len(coefficients) / 4), yearly_predictor, months
@@ -36,9 +43,14 @@ def test_fit_to_bic_np(coefficients):
     selected_order, estimated_coefficients, predictions = fit_to_bic_np(
         yearly_predictor, monthly_target, max_order=6
     )
+    
+    # assert selected_order == int(len(coefficients) / 4) 
+    # the model does not necessarily select the "correct" order
+    # but the coefficients of higher orders should be close to zero
 
-    assert selected_order == int(len(coefficients) / 4)
-    np.testing.assert_allclose(
-        estimated_coefficients[0 : selected_order * 4], coefficients, atol=1e-2
-    )
-    np.testing.assert_allclose(predictions, monthly_target, atol=1e-2)
+    # linear combination of the coefficients with the predictor should be similar
+    np.testing.assert_allclose([coefficients[i]*yearly_predictor[i]+coefficients[i+1] for i in range(0, len(coefficients), 2)],
+                           [estimated_coefficients[i]*yearly_predictor[i]+estimated_coefficients[i+1] for i in range(0, len(estimated_coefficients), 2)], atol=1e-2)
+
+    # actually all what really counts is that the predictions are close to the target
+    np.testing.assert_allclose(predictions, monthly_target, atol=1e-1)
