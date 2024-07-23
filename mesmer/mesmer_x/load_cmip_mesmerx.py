4ad7bb2e5ec901cd16d95d0250ac6552fbc77c1b
veni-vidi-vici-dormivi <victoria.bauer@env.ethz.ch>
2024-07-23 13:44:37 +0200

    remove double hashtags

--- mesmer/mesmer_x/load_cmip_mesmerx.py
+++ mesmer/mesmer_x/load_cmip_mesmerx.py
@@ -94,14 +94,14 @@ def find_files_cmip(esm, var, scenario, cfg):
     # identifying correct arguments to look for file: directory name, exclusions, member_style, !!
     if var in ["tas", "hfds", "pr"]:
         if cfg.gen == 5:
-            ## directory name CMIP5-ng
+            # directory name CMIP5-ng
             dir_name = cfg.dir_cmipng + var + "/"
 
-            ## preparing search of files
+            # preparing search of files
             prefix_path_file = dir_name + var + "_ann_" + esm + "_"
             suffix_path_file = "_" + "r*i1p1" + "_g025.nc"
 
-            ## exclusions CMIP5-ng
+            # exclusions CMIP5-ng
             if var == "tas":
                 esms_excl = ["GISS-E2-H"]  # list of all ESMs which have excluded runs
                 runs_excl = [
@@ -113,14 +113,14 @@ def find_files_cmip(esm, var, scenario, cfg):
                 print("TO DO: create list of excluded runs / ESMs for this variable")
 
         elif cfg.gen == 6:
-            ## directory name CMIP6-ng
+            # directory name CMIP6-ng
             dir_name = cfg.dir_cmipng + var + "/ann/g025/"
 
-            ## preparing search of files
+            # preparing search of files
             prefix_path_file = dir_name + var + "_ann_" + esm + "_"
             suffix_path_file = "_" + "r*i1p1f*" + "_g025.nc"
 
-            ## exclusions CMIP6-ng
+            # exclusions CMIP6-ng
             if var == "tas":
                 esms_excl = [
                     "ACCESS-ESM1-5",
@@ -212,7 +212,7 @@ def find_files_cmip(esm, var, scenario, cfg):
         order[int(memb[: memb.find("i")])] = s
     sorted_order = list(order.keys())
     sorted_order.sort(reverse=False)
-    ## putting at the end in the correct order, removing the first occurence
+    # putting at the end in the correct order, removing the first occurence
     for i_s in sorted_order:
         path_runs_list.append(order[i_s])
         path_runs_list.remove(order[i_s])
@@ -288,7 +288,7 @@ def load_cmip(targ, esm, scen, cfg):
     for run_path in path_runs_list:
         data, run = load_cmip_file(run_path, _scen, cfg)
 
-        ## cutting file to the requested period
+        # cutting file to the requested period
         data = data.sel(
             time=slice(
                 max([data.time.values[0], float(cfg.time["start"])]),
@@ -327,22 +327,22 @@ def load_cmip(targ, esm, scen, cfg):
                 .values,
             ]
 
-    ## preparing references
-    if cfg.ref["type"] == "all":  ## averaging the reference over all
+    # preparing references
+    if cfg.ref["type"] == "all":  # averaging the reference over all
         var_ref = [
             sum([refs[run_path][i] for run_path in path_runs_list])
             / len(path_runs_list)
             for i in range(refs[run])
         ]
-    elif cfg.ref["type"] == "first":  ## choosing only the first one
+    elif cfg.ref["type"] == "first":  # choosing only the first one
         var_ref = refs["1"]
     elif cfg.ref["type"] in [
         "individ",
         "none",
-    ]:  ## no need to calculate a global reference
+    ]:  # no need to calculate a global reference
         pass
 
-    ## computing anomalies
+    # computing anomalies
     for run in var.keys():
         if targ in [
             "mrso",
@@ -355,7 +355,7 @@ def load_cmip(targ, esm, scen, cfg):
         ]:
             # Not removing anomaly or normalizing soil moisture.
             pass
-            ## Not removing anomaly or normalizing soil moisture. In SMA, normalizing
+            # Not removing anomaly or normalizing soil moisture. In SMA, normalizing
             # if cfg.ref["type"] in ["all","first"]:## removing precalculated reference
             #    var[run] -= var_ref[0]
             #    var[run] /= var_ref[1]
@@ -368,11 +368,11 @@ def load_cmip(targ, esm, scen, cfg):
             #    raise Exception('Unprepared reference type: '+cfg.ref["type"])
 
         else:
-            if cfg.ref["type"] in ["all", "first"]:  ## removing precalculated reference
+            if cfg.ref["type"] in ["all", "first"]:  # removing precalculated reference
                 var[run] -= var_ref[0]
-            elif cfg.ref["type"] == "individ":  ## individual references
+            elif cfg.ref["type"] == "individ":  # individual references
                 var[run] -= refs[run][0]
-            elif cfg.ref["type"] == "none":  ## not calculating anomalies
+            elif cfg.ref["type"] == "none":  # not calculating anomalies
                 pass
             else:
                 raise Exception("Unprepared reference type: " + cfg.ref["type"])
@@ -384,13 +384,13 @@ def load_cmip(targ, esm, scen, cfg):
     GVAR = {}
     for run_path in path_runs_list:
         run = run_nrs[run_path]
-        if targ in ["hfds"]:  ## specific case
+        if targ in ["hfds"]:  # specific case
             masked_var = np.ma.masked_array(
                 var[run], np.isnan(var[run])
-            )  ## account for missing values over land
+            )  # account for missing values over land
             GVAR[run] = np.average(
                 masked_var, axis=(1, 2), weights=wgt3d
-            )  ## no need to keep mask since False everywhere
+            )  # no need to keep mask since False everywhere
         else:
             GVAR[run] = np.average(var[run], axis=(1, 2), weights=wgt3d)
 
@@ -428,7 +428,7 @@ def load_cmip_file(run_path, scen, cfg):
             lon=(((data.lon + 180) % 360) - 180)
         )  # assign_coords so that labels = reasonable
 
-        ## update here the time axis of the files for more robust combine_first, due to different time formats.
+        # update here the time axis of the files for more robust combine_first, due to different time formats.
         data.coords["time"] = update_time_axis(data.time.values, run_path)
 
         run = int(
@@ -437,14 +437,14 @@ def load_cmip_file(run_path, scen, cfg):
 
     elif cfg.gen == 6:
         if "ssp534-over" in run_path:
-            ## Solving two issues here :
-            ## Issue of ssp534-over: it starts in 2040, before it follows ssp585: cut in 2039
-            ## Issue of the cmip_X repository: some historicals overlap over the scenario period. Choosing here that the scenario determines values over the overlap.
+            # Solving two issues here :
+            # Issue of ssp534-over: it starts in 2040, before it follows ssp585: cut in 2039
+            # Issue of the cmip_X repository: some historicals overlap over the scenario period. Choosing here that the scenario determines values over the overlap.
             ssp534over = xr.open_dataset(run_path, use_cftime=True)
             ssp585 = xr.open_dataset(run_path.replace(scen, "ssp585"), use_cftime=True)
             hh = xr.open_dataset(run_path.replace(scen, "historical"), use_cftime=True)
 
-            ## update here the time axis of the files for more robust combine_first, due to different time formats.
+            # update here the time axis of the files for more robust combine_first, due to different time formats.
             ssp534over.coords["time"] = update_time_axis(
                 ssp534over.time.values, run_path
             )
@@ -455,23 +455,23 @@ def load_cmip_file(run_path, scen, cfg):
                 hh.time.values, run_path.replace(scen, "historical")
             )
 
-            #### Order of scenarios: historical (1850-2014) - ssp585 (2015-2039) - ssp534-over (2040-...)
+            # Order of scenarios: historical (1850-2014) - ssp585 (2015-2039) - ssp534-over (2040-...)
             data = ssp534over.combine_first(
                 ssp585.sel(time=slice(None, "2039")).combine_first(hh)
             )
 
         else:  # for every other scenario
-            ## Issue of the cmip_X repository: some historicals overlap over the scenario period. Choosing here that the scenario determines values over the overlap.
+            # Issue of the cmip_X repository: some historicals overlap over the scenario period. Choosing here that the scenario determines values over the overlap.
             ssp = xr.open_dataset(run_path, use_cftime=True)
             hh = xr.open_dataset(run_path.replace(scen, "historical"), use_cftime=True)
 
-            ## update here the time axis of the files for more robust combine_first, due to different time formats.
+            # update here the time axis of the files for more robust combine_first, due to different time formats.
             ssp.coords["time"] = update_time_axis(ssp.time.values, run_path)
             hh.coords["time"] = update_time_axis(
                 hh.time.values, run_path.replace(scen, "historical")
             )
 
-            ## Choosing here that the scenario determines values over the overlap.
+            # Choosing here that the scenario determines values over the overlap.
             data = ssp.combine_first(hh)
 
         data = data.roll(lon=72, roll_coords=True)  # roll so land in center
@@ -515,7 +515,7 @@ def update_time_axis(time_axis, run_path):
                 val.year,
                 val.month,
                 val.day,
-            )  ## nothing to do, can keep "val" this way
+            )  # nothing to do, can keep "val" this way
         elif type(val) in [np.datetime64]:
             val = pd.DatetimeIndex([val])
             yr, mon, day = val[0].year, val[0].month, val[0].day
@@ -529,10 +529,10 @@ def update_time_axis(time_axis, run_path):
 
         if np.all(
             np.isnan([yr, mon, day])
-        ):  ## some of cmip5_X files have np.datetime64 'NaT' instead of a valid format, but values are still provided by the ESM. To exclude?
+        ):  # some of cmip5_X files have np.datetime64 'NaT' instead of a valid format, but values are still provided by the ESM. To exclude?
             tm.append(
                 [tm[-1][0] + 1, tm[-1][1], tm[-1][2]]
-            )  ## adding last year+1, same month and day.
+            )  # adding last year+1, same month and day.
         else:
             tm.append([yr, mon, day])
     if np.any(np.diff(np.array(tm)[:, 1]) != 0.0) or np.any(
@@ -563,7 +563,7 @@ def test_combination_vars(list_vars, esm, scen, cfg):
     dico_runs : dict
         dictionary with the paths for all variables
     """
-    ## check if scenario is correctly defined as historical + scenario
+    # check if scenario is correctly defined as historical + scenario
     if scen[:2] == "h-":
         _scen = scen[2:]
     else:
@@ -571,19 +571,19 @@ def test_combination_vars(list_vars, esm, scen, cfg):
             "No version without historical time period is currently implemented. Crashing this function."
         )
 
-    ## looping on all variables to get their respective list of paths
+    # looping on all variables to get their respective list of paths
     dico_paths = {}
     for targ in list_vars:
         ## find the files which fulfill the specifications
         dico_paths[targ] = find_files_cmip(esm, targ, _scen, cfg)
 
-    ## deducing available runs for each
+    # deducing available runs for each
     dico_runs = {
         targ: [str.split(run, "_")[-2] for run in dico_paths[targ]]
         for targ in list_vars
     }
 
-    ## checking that at least one of the runs is available for all:
+    # checking that at least one of the runs is available for all:
     common_runs = []
     for run in dico_runs[list_vars[0]]:
         if np.all([run in dico_runs[var] for var in list_vars]):
