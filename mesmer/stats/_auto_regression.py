a1786d24390b86c6bd2441591433a8642eb3d120
veni-vidi-vici-dormivi <victoria.bauer@env.ethz.ch>
2024-08-15 11:45:55 +0200

    move rng up

--- mesmer/stats/_auto_regression.py
+++ mesmer/stats/_auto_regression.py
@@ -475,8 +475,11 @@ def _draw_auto_regression_correlated_np(
     # arbitrary lags? no, see: https://github.com/MESMER-group/mesmer/issues/164
     ar_lags = np.arange(1, ar_order + 1, dtype=int)
 
+    # ensure reproducibility
+    rng = np.random.default_rng(seed)
+
     innovations = _draw_innovations_correlated_np(
-        covariance, seed, n_coeffs, n_samples, n_ts, buffer
+        covariance, rng, n_coeffs, n_samples, n_ts, buffer
     )
 
     out = np.zeros([n_samples, n_ts + buffer, n_coeffs])
@@ -490,7 +493,7 @@ def _draw_auto_regression_correlated_np(
 
 
 def _draw_innovations_correlated_np(
-    covariance, seed, n_gridcells, n_samples, n_ts, buffer
+    covariance, rng, n_gridcells, n_samples, n_ts, buffer
 ):
     # NOTE: 'innovations' is the error or noise term.
     # innovations has shape (n_samples, n_ts + buffer, n_coeffs)
@@ -505,8 +508,6 @@ def _draw_innovations_correlated_np(
                 LinAlgWarning,
             )
 
-    # ensure reproducibility
-    rng = np.random.default_rng(seed)
     innovations = scipy.stats.multivariate_normal.rvs(
         mean=np.zeros(n_gridcells),
         cov=cov,
@@ -881,13 +882,16 @@ def _draw_auto_regression_monthly_np(
 
     _, n_gridcells = intercept.shape
 
+    # ensure reproducibility
+    rng = np.random.default_rng(seed)
+
     # draw innovations for each month
     innovations = np.zeros([n_samples, n_ts // 12 + buffer, 12, n_gridcells])
     if covariance is not None:
         for month in range(12):
             cov_month = covariance[month, :, :]
             innovations[:, :, month, :] = _draw_innovations_correlated_np(
-                cov_month, seed, n_gridcells, n_samples, n_ts // 12, buffer
+                cov_month, rng, n_gridcells, n_samples, n_ts // 12, buffer
             )
     # reshape innovations into continuous time series
     innovations = innovations.reshape(n_samples, n_ts + buffer * 12, n_gridcells)
