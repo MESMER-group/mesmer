8c65c8245d4113b476f722352d3a7ca03c494121
Victoria <112418493+veni-vidi-vici-dormivi@users.noreply.github.com>
2024-08-22 08:42:31 +0200

    Update mesmer/calibrate_mesmer/train_lv.py
    Co-authored-by: Mathias Hauser <mathause@users.noreply.github.com>

--- mesmer/calibrate_mesmer/train_lv.py
+++ mesmer/calibrate_mesmer/train_lv.py
@@ -234,7 +234,7 @@ def train_lv_AR1_sci(params_lv, targs, y, wgt_scen_eq, aux, cfg):
         data = [
             xr.DataArray(
                 data,
-                dims=dims,
+                dims=dims
             )
             for data in targ.values()
         ]
