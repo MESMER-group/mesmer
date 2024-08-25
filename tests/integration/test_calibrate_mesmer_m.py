ab9f0b1fccfacf46d55a235a806d10d83a7e4c67
veni-vidi-vici-dormivi <victoria.bauer@env.ethz.ch>
2024-08-25 16:45:48 +0200

    forgot false

--- tests/integration/test_calibrate_mesmer_m.py
+++ tests/integration/test_calibrate_mesmer_m.py
@@ -7,7 +7,7 @@ import xarray as xr
 import mesmer
 
 
-def test_calibrate_mesmer_m(update_expected_files=True):
+def test_calibrate_mesmer_m(update_expected_files=False):
     # define config values
     THRESHOLD_LAND = 1 / 3
 
