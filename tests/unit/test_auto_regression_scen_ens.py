fb1eae2a72ab0c86017ff88a052e3c333c2dd374
veni-vidi-vici-dormivi <victoria.bauer@env.ethz.ch>
2024-12-05 16:15:31 +0100

    rename format_wrapper

--- tests/unit/test_auto_regression_scen_ens.py
+++ tests/unit/test_auto_regression_scen_ens.py
@@ -40,7 +40,7 @@ def _prepare_data(*dataarrays, data_format):
         raise ValueError(f"Unknown format_type: {data_format}")
 
 
-def _format_wrapper(func: Callable, data_format: str, data, **kwargs):
+def _maybe_unpack_data_and_call(func: Callable, data_format: str, data, **kwargs):
     if data_format == "tuple":
         return func(*data, **kwargs)
     else:
@@ -53,7 +53,7 @@ def test_select_ar_order_scen_ens_one_scen(data_format):
     da = generate_ar_samples([1, 0.5, 0.3, 0.4], n_timesteps=100, n_ens=4)
     data = _prepare_data(da, data_format=data_format)
 
-    result = _format_wrapper(
+    result = _maybe_unpack_data_and_call(
         mesmer.stats.select_ar_order_scen_ens,
         data_format,
         data,
@@ -75,7 +75,7 @@ def test_select_ar_order_scen_ens_multi_scen_tuple(data_format):
 
     data = _prepare_data(da1, da2, data_format=data_format)
 
-    result = _format_wrapper(
+    result = _maybe_unpack_data_and_call(
         mesmer.stats.select_ar_order_scen_ens,
         data_format,
         data,
@@ -95,7 +95,7 @@ def test_select_ar_order_scen_ens_no_ens_dim(data_format):
     da = generate_ar_samples([1, 0.5, 0.3, 0.4], n_timesteps=100, n_ens=4)
     data = _prepare_data(da, data_format=data_format)
 
-    result = _format_wrapper(
+    result = _maybe_unpack_data_and_call(
         mesmer.stats.select_ar_order_scen_ens,
         data_format,
         data,
@@ -120,7 +120,7 @@ def test_fit_auto_regression_scen_ens_one_scen(data_format, std):
     da = generate_ar_samples([1, 0.5, 0.3, 0.4], std, n_timesteps=n_timesteps, n_ens=4)
     data = _prepare_data(da, data_format=data_format)
 
-    result = _format_wrapper(
+    result = _maybe_unpack_data_and_call(
         mesmer.stats.fit_auto_regression_scen_ens,
         data_format,
         data,
@@ -143,7 +143,7 @@ def test_fit_auto_regression_scen_ens_multi_scen(data_format):
 
     data = _prepare_data(da1, da2, data_format=data_format)
 
-    result = _format_wrapper(
+    result = _maybe_unpack_data_and_call(
         mesmer.stats.fit_auto_regression_scen_ens,
         data_format,
         data,
@@ -168,7 +168,7 @@ def test_fit_auto_regression_scen_ens_no_ens_dim(data_format):
     data = _prepare_data(da, data_format=data_format)
 
     # simply fits each ens individually, no averaging
-    result = _format_wrapper(
+    result = _maybe_unpack_data_and_call(
         mesmer.stats.fit_auto_regression_scen_ens,
         data_format,
         data,
