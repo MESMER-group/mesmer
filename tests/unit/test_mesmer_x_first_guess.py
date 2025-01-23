5910c5551f2ee1fab446c6aef8bf99687f8fdca2
veni-vidi-vici-dormivi <victoria.bauer@env.ethz.ch>
2025-01-23 11:19:12 +0100

    add tests for beta and gamma distribution

--- tests/unit/test_mesmer_x_first_guess.py
+++ tests/unit/test_mesmer_x_first_guess.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pytest
-from scipy.stats import genextreme, laplace, truncnorm
+from scipy.stats import beta, gamma, genextreme, laplace, truncnorm
 
 from mesmer.mesmer_x import Expression, distrib_cov
 from mesmer.mesmer_x.train_l_distrib_mesmerx import _smooth_data
@@ -142,6 +142,70 @@ def test_first_guess_truncnorm():
     expected = [loc, scale, a, b]
 
     np.testing.assert_allclose(result, expected, rtol=0.52)
+
+
+def test_first_guess_beta():
+    rng = np.random.default_rng(0)
+    n = 251
+    pred = np.ones(n)
+
+    a = 2
+    b = 2
+    loc = 0
+    scale = 1
+    targ = beta.rvs(a, b, loc, scale, size=n, random_state=rng)
+
+    expression = Expression("beta(loc=0, scale=1, a=c3, b=c4)", expr_name="exp1")
+
+    # we need a first guess here because our default first guess is zeros, which leads
+    # to a degenerate distribution in the case of a beta distribution
+    first_guess = [1.0, 1.0]
+    options_solver = {"fg_with_global_opti": True}
+    dist = distrib_cov(
+        targ,
+        {"tas": pred},
+        expression,
+        first_guess=first_guess,
+        options_solver=options_solver,
+    )
+    dist.find_fg()
+
+    # NOTE: for the beta distribution the support does not change for loc = 0 and scale = 1
+    # it is always (0,1), thus the optimization with _fg_fun_others does not do anything
+    # NOTE: Step 7 (fg_with_global_opti) leads to a impovement of the first guess at the 6th digit after the comma, i.e. very small
+    result = dist.fg_coeffs
+    expected = [a, b]
+
+    np.testing.assert_allclose(result, expected, rtol=0.5)
+
+
+def test_first_guess_gamma():
+    rng = np.random.default_rng(0)
+    n = 251
+    pred = np.ones(n)
+
+    a = 2
+    loc = 0
+    scale = 1
+    targ = gamma.rvs(a, loc, scale, size=n, random_state=rng)
+
+    expression = Expression("gamma(loc=0, scale=1, a=c1)", expr_name="exp1")
+
+    # we need a first guess different from zero for gamma distribution
+    first_guess = [1.0]
+    options_solver = {"fg_with_global_opti": True}
+    dist = distrib_cov(
+        targ,
+        {"tas": pred},
+        expression,
+        first_guess=first_guess,
+        options_solver=options_solver,
+    )
+    dist.find_fg()
+    result = dist.fg_coeffs
+    expected = [a]
+
+    np.testing.assert_allclose(result, expected, atol=0.03)
 
 
 def test_fg_fun_scale_laplace():
