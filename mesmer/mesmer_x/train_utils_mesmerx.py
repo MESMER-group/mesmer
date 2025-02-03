c4bf0dc9f28b813dcc1858d28f3386fa6babf12f
Mathias Hauser <mathias.hauser@env.ethz.ch>
2025-02-03 22:38:10 +0100

    MESMER-X: keep scalars

--- mesmer/mesmer_x/train_utils_mesmerx.py
+++ mesmer/mesmer_x/train_utils_mesmerx.py
@@ -370,28 +370,28 @@ class Expression:
             parameters_values[param] = eval(expr, None, locals)
 
         # Correcting shapes 1: scalar parameters must have the shape of the inputs
-        if len(self.inputs_list) > 0:
+        # if len(self.inputs_list) > 0:
 
-            for param in self.parameters_list:
-                param_value = parameters_values[param]
+        #     for param in self.parameters_list:
+        #         param_value = parameters_values[param]
 
-                # TODO: use np.ndim(param_value) ==  0? (i.e. isscalar)
-                if isinstance(param_value, int | float) or param_value.ndim == 0:
-                    if isinstance(coefficients_values, xr.Dataset) and (
-                        isinstance(inputs_values, xr.Dataset)
-                    ):
-                        param_value = param_value * xr.ones_like(
-                            inputs_values[self.inputs_list[0]]
-                        )
-                    else:
-                        param_value = param_value * np.ones(
-                            inputs_values[self.inputs_list[0]].shape
-                        )
+        #         # TODO: use np.ndim(param_value) ==  0? (i.e. isscalar)
+        #         if isinstance(param_value, int | float) or param_value.ndim == 0:
+        #             if isinstance(coefficients_values, xr.Dataset) and (
+        #                 isinstance(inputs_values, xr.Dataset)
+        #             ):
+        #                 param_value = param_value * xr.ones_like(
+        #                     inputs_values[self.inputs_list[0]]
+        #                 )
+        #             else:
+        #                 param_value = param_value * np.ones(
+        #                     inputs_values[self.inputs_list[0]].shape
+        #                 )
 
-                parameters_values[param] = param_value
+        #         parameters_values[param] = param_value
 
         # Correcting shapes 2: possibly forcing shape
-        if len(self.inputs_list) > 0 and forced_shape is not None:
+        if forced_shape is not None and len(self.inputs_list) > 0:
 
             for param in self.parameters_list:
                 dims_param = [
