b380463488af1b4a10ebda5909281a93a7f84c36
Mathias Hauser <mathias.hauser@env.ethz.ch>
2025-04-04 12:06:59 +0200

    map_over_datasets: add docstring

--- mesmer/core/_datatreecompat.py
+++ mesmer/core/_datatreecompat.py
@@ -21,6 +21,37 @@ def skip_empty_nodes(func):
 
 
 def map_over_datasets(func, *args, kwargs=None):
+    """
+    Applies a function to every dataset in one or more DataTree objects with
+    the same structure (ie.., that are isomorphic), returning new trees which
+    store the results.
+
+    adapted version of xr.map_over_datasets which skips empty nodes
+
+    Parameters
+    ----------
+    func : callable
+        Function to apply to datasets with signature:
+
+        `func(*args: Dataset, **kwargs) -> Union[Dataset, tuple[Dataset, ...]]`.
+
+        (i.e. func must accept at least one Dataset and return at least one Dataset.)
+    *args : tuple, optional
+        Positional arguments passed on to `func`. Any DataTree arguments will be
+        converted to Dataset objects via `.dataset`.
+    kwargs : dict, optional
+        Optional keyword arguments passed directly to ``func``.
+
+
+    See Also
+    --------
+    xr.map_over_datasets
+
+    Notes
+    -----
+    For the discussion in xarray see https://github.com/pydata/xarray/issues/9693
+
+    """
 
     return xr.map_over_datasets(skip_empty_nodes(func), *args, kwargs=kwargs)
 
