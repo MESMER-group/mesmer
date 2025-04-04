2d50613c8ceffa00d50ea28a9ca5d221624cc099
Mathias Hauser <mathias.hauser@env.ethz.ch>
2025-04-04 11:05:44 +0200

    comment POLICY_OVERRIDE

--- ci/min_deps_check.py
+++ ci/min_deps_check.py
@@ -34,6 +34,7 @@ IGNORE_DEPS = {
 
 POLICY_MONTHS = {"python": 30, "numpy": 18}
 POLICY_MONTHS_DEFAULT = 12
+# xarray v2025.03 is needed for xr.DataTree support
 POLICY_OVERRIDE: dict[str, tuple[int, int]] = {"xarray": (2025, 3)}
 errors = []
 
