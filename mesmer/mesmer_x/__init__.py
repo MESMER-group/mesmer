d4b8049833110d7897e934ccbeaec9cf1152b9cd
veni-vidi-vici-dormivi <victoria.bauer@env.ethz.ch>
2025-05-06 18:24:44 +0200

    update mesmer_x init

--- mesmer/mesmer_x/__init__.py
+++ mesmer/mesmer_x/__init__.py
@@ -1,12 +1,26 @@
-# MESMER, land-climate dynamics group, S.I. Seneviratne
-# Copyright (c) 2021 ETH Zurich, MESMER contributors listed in AUTHORS.
-# Licensed under the GNU General Public License v3.0 or later see LICENSE or
-# https://www.gnu.org/licenses/
-"""
-Collection of functions related to MESMER-X.
-"""
+from mesmer.mesmer_x._conditional_distribution import ConditionalDistribution
+from mesmer.mesmer_x._distrib_tests import distrib_tests
+from mesmer.mesmer_x._expression import Expression
+from mesmer.mesmer_x._first_guess import distrib_firstguess
+from mesmer.mesmer_x._optimizers import distrib_optimizer
+from mesmer.mesmer_x._probability_integral_transform import probability_integral_transform, weighted_median
+from mesmer.mesmer_x._weighting import get_weights_density, get_weights_uniform
 
-# flake8: noqa
-# TODO: replace * imports
-from .train_l_distrib_mesmerx import *
-from .train_utils_mesmerx import *
+__all__ = [
+    # conditional distribution
+    "ConditionalDistribution",
+    # tests
+    "distrib_tests",
+    # expression
+    "Expression",
+    # first guess
+    "distrib_firstguess",
+    # optimizers
+    "distrib_optimizer",
+    # probability integral transform
+    "probability_integral_transform",
+    "weighted_median",
+    # weighting
+    "get_weights_density",
+    "get_weights_uniform",
+]
