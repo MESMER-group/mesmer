e2e3cc9f79191a0be9fe43a3ad6535b47ec1c163
veni-vidi-vici-dormivi <victoria.bauer@env.ethz.ch>
2025-06-20 18:45:29 +0200

    update drawing realisations test and data

--- tests/integration/test_draw_realisations_mesmer_x.py
+++ tests/integration/test_draw_realisations_mesmer_x.py
@@ -10,20 +10,20 @@ from mesmer.mesmer_x import (
     Expression,
     ProbabilityIntegralTransform,
 )
-
+from filefisher import FileFinder
 
 @pytest.mark.parametrize(
-    ("scenario", "target_name", "expr_name", "update_expected_files"),
+    ("scenario", "targ_var", "expr_name", "update_expected_files"),
     [
         pytest.param(
-            "ssp585",
+            "ssp126",
             "tasmax",
             "expr1",
             False,
             marks=pytest.mark.slow,
         ),
         pytest.param(
-            "ssp585",
+            "ssp126",
             "tasmax",
             "expr1_2ndfit",
             False,
@@ -32,7 +32,7 @@ from mesmer.mesmer_x import (
     ],
 )
 def test_make_realisations_mesmer_x(
-    scenario, target_name, expr_name, test_data_root_dir, update_expected_files
+    scenario, targ_var, expr_name, test_data_root_dir, update_expected_files
 ):
     # set some configuration parameters
     n_realizations = 1  # TODO: can now do more than 1 realization. change output data?
@@ -42,7 +42,7 @@ def test_make_realisations_mesmer_x(
 
     # load data
     TEST_DATA_PATH = pathlib.Path(test_data_root_dir)
-    TEST_PATH = TEST_DATA_PATH / "output" / target_name / "one_scen_one_ens"
+    TEST_PATH = TEST_DATA_PATH / "output" / targ_var / "one_scen_one_ens"
     cmip6_data_path = mesmer.example_data.cmip6_ng_path()
 
     # load predictor data
@@ -57,7 +57,6 @@ def test_make_realisations_mesmer_x(
     tas_ssp585 = xr.open_dataset(fN_ssp585, decode_times=time_coder).drop_vars(
         ["height", "file_qf", "time_bnds"]
     )
-    # tas = DataTree({"hist": tas_hist, "ssp585": tas_ssp585})
 
     # make global mean
     # global_mean_dt = map_over_datasets(mesmer.weighted.global_mean)
@@ -69,27 +68,38 @@ def test_make_realisations_mesmer_x(
     time = predictor.time
 
     # load the parameters
-    file_end = f"{target_name}_{expr_name}_{esm}_{scenario}"
-    distrib_orig = ConditionalDistribution.from_netcdf(
-        TEST_PATH
-        / "test-params"
-        / "distrib"
-        / f"params_transform_distrib_{file_end}.nc"
+    PARAM_FILEFINDER = FileFinder(
+        path_pattern=TEST_PATH / "test-params/{module}/",
+        file_pattern="params_{module}_{targ_var}_{expr_name}_{esm}_{scen}.nc",
     )
 
-    local_ar_params = xr.open_dataset(
-        TEST_PATH
-        / "test-params"
-        / "local_variability"
-        / f"params_local_AR_{file_end}.nc"
-    )
+    distrib_file = PARAM_FILEFINDER.find_single_file(
+        module="distrib",
+        targ_var=targ_var,
+        expr_name=expr_name,
+        esm=esm,
+        scen=scenario,
+    ).paths.pop()
+    local_ar_file = PARAM_FILEFINDER.find_single_file(
+        module="local_trends",
+        targ_var=targ_var,
+        expr_name=expr_name,
+        esm=esm,
+        scen=scenario,
+    ).paths.pop()
+    localized_ecov_file = PARAM_FILEFINDER.find_single_file(
+        module="local_variability",
+        targ_var=targ_var,
+        expr_name=expr_name,
+        esm=esm,
+        scen=scenario,
+    ).paths.pop()
+    
+    distrib_orig = ConditionalDistribution.from_netcdf(distrib_file)
 
-    localized_ecov = xr.open_dataset(
-        TEST_PATH
-        / "test-params"
-        / "local_variability"
-        / f"params_localized_ecov_{file_end}.nc"
-    )
+    local_ar_params = xr.open_dataset(local_ar_file)
+
+    localized_ecov = xr.open_dataset(localized_ecov_file)
 
     # generate realizations based on the auto-regression with spatially correlated innovations
     transf_emus = mesmer.stats.draw_auto_regression_correlated(
@@ -100,7 +110,7 @@ def test_make_realisations_mesmer_x(
         seed=seed,
         buffer=buffer,
     )
-    transf_emus = transf_emus.rename({"samples": target_name})
+    transf_emus = transf_emus.rename({"samples": targ_var})
 
     # back-transform the realizations
     expr_tranf = Expression("norm(loc=0, scale=1)", "standard_normal")
@@ -114,7 +124,7 @@ def test_make_realisations_mesmer_x(
     )
 
     emus = back_pit.transform(
-        data=transf_emus, target_name=target_name, preds_orig=None, preds_targ=predictor
+        data=transf_emus, target_name=targ_var, preds_orig=None, preds_targ=predictor
     )
 
     expected_output_file = (
@@ -128,7 +138,7 @@ def test_make_realisations_mesmer_x(
         # load the output
         time_coder = xr.coders.CFDatetimeCoder(use_cftime=True)
         expected_emus = xr.open_dataarray(expected_output_file, decode_times=time_coder)
-        xr.testing.assert_allclose(emus[target_name], expected_emus)
+        xr.testing.assert_allclose(emus[targ_var], expected_emus)
 
         # make sure we can get onto a lat lon grid from what is saved
         exp_reshaped = expected_emus.set_index(gridpoint=("lat", "lon")).unstack(
