e45f3eaf3d1f4646953cc01675caf7e98412724e
veni-vidi-vici-dormivi <victoria.bauer@env.ethz.ch>
2025-06-20 22:11:52 +0200

    allow nans in weighted median

--- mesmer/core/weighted.py
+++ mesmer/core/weighted.py
@@ -341,16 +341,20 @@ def weighted_median(data, weights):
       https://gist.github.com/tinybike/d9ff1dad515b66cc0d87
       @author Jack Peterson (jack@tinybike.net)
     """
+
+    weights = weights[~np.isnan(data)]
+    data = data[~np.isnan(data)]
+    
     data, weights = np.asarray(data).squeeze(), np.asarray(weights).squeeze()
     s_data, s_weights = map(np.array, zip(*sorted(zip(data, weights))))
-    midpoint = 0.5 * sum(s_weights)
+    midpoint = 0.5 * np.sum(s_weights)
 
     if any(weights > midpoint):
         w_median = (data[weights == np.max(weights)])[0]
     else:
         cs_weights = np.cumsum(s_weights)
         idx = np.where(cs_weights <= midpoint)[0][-1]
-        if cs_weights[idx] == midpoint:
+        if any(cs_weights == midpoint):
             w_median = np.mean(s_data[idx : idx + 2])
         else:
             w_median = s_data[idx + 1]
