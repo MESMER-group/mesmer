eaee1021e9d81bac03eda92c0da0120c4824af59
veni-vidi-vici-dormivi <victoria.bauer@env.ethz.ch>
2025-07-29 17:28:54 +0200

    shallow copy innovations into out for draw_auto_regression_monthly_np

--- mesmer/stats/_auto_regression.py
+++ mesmer/stats/_auto_regression.py
@@ -1167,7 +1167,9 @@ def _draw_auto_regression_monthly_np(
     innovations = innovations.reshape(n_samples, n_ts + buffer * 12, n_gridcells)
 
     # predict auto-regressive process using innovations
-    out = np.zeros([n_samples, n_ts + buffer * 12, n_gridcells])
+    out = (
+        innovations  # copy-by-reference: use innovations as out param to save on memory
+    )
     for t in range(1, n_ts + buffer * 12):
         month = t % 12
         out[:, t, :] = (
