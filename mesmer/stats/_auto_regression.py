ba27c4ea6fb43a61bd8e5565619629676b9c0fde
veni-vidi-vici-dormivi <victoria.bauer@env.ethz.ch>
2025-07-29 17:30:56 +0200

    move comment to avoid line break

--- mesmer/stats/_auto_regression.py
+++ mesmer/stats/_auto_regression.py
@@ -1167,9 +1167,8 @@ def _draw_auto_regression_monthly_np(
     innovations = innovations.reshape(n_samples, n_ts + buffer * 12, n_gridcells)
 
     # predict auto-regressive process using innovations
-    out = (
-        innovations  # copy-by-reference: use innovations as out param to save on memory
-    )
+    # copy-by-reference: use innovations as out param to save on memory
+    out = innovations
     for t in range(1, n_ts + buffer * 12):
         month = t % 12
         out[:, t, :] = (
